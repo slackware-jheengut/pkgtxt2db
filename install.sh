ab62038c8e3b5e025e4f39af5bf277a5e2f46230
frédéric galusik <fredg@salixos.org>
2012-11-01 20:40:03 +0100

    updated to change from Perl to Python

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-PERLVER=5.12.3
-PERLDIR=usr/lib${LIBDIRSUFFIX}/perl5/$PERLVER
+install -D -m 755 pkgtxt2db.py $DESTDIR/usr/bin/pkgtxt2db
 
-install -D -m 755 pkgtxt2db $DESTDIR/usr/bin/pkgtxt2db
-install -D -m 644 Pkgtxt2db.pm $DESTDIR/$PERLDIR/Pkgtxt2db/Pkgtxt2db.pm
+# man page
+txt2tags pkgtxt2db.t2t || return 1
 install -D -m 644 pkgtxt2db.man $DESTDIR/usr/man/man1/pkgtxt2db.1
-
